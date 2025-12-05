943e4ec0d3a96781661684f22c27c84698c31da2
aquoc09 <anhquoc6a4@gmail.com>
2025-12-06 01:36:51 +0700

    fix call back

--- auth/google_callback.php
+++ auth/google_callback.php
@@ -9,7 +9,7 @@ include '../application_config.php';
 $client = new Google\Client;
 $client->setClientId(GOOGLE_CLIENT_ID);
 $client->setClientSecret(GOOGLE_CLIENT_SECRET);
-$client->setRedirectUri("http://localhost/web_order_php/auth/google_callback.php");
+$client->setRedirectUri(GOOGLE_REDIRECT_URI);
 
 $client->addScope("email");
 $client->addScope("profile");
