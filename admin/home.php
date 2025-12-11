d789a9874bfef77ce2bdf94c3a67a8429f1979e2
aquoc09 <anhquoc6a4@gmail.com>
2025-12-11 22:01:47 +0700

    update revenue

--- admin/home.php
+++ admin/home.php
@@ -1,12 +1,10 @@
 
-
 <div class="main-content">
     <div class="main-content-inner">
         <div class="container">
                 <div class="box">
                     <h1>Chào mừng đến hệ thống quản lý!</h1>
                     <p>Bạn có thể xem doanh thu, quản lý đơn hàng, khách hàng và nhiều hơn nữa.</p>
-                    <a href="revenue.php" class="button">Xem doanh thu</a>
                 </div>
         </div>
     </div>
