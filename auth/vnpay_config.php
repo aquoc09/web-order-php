57cda891b678b2ca49e32fbe6f88af2fd6e3af04
Hyuga31415 <darkness1652004@gmail.com>
2025-12-13 08:12:21 +0700

    fix vnpay_config

--- auth/vnpay_config.php
+++ auth/vnpay_config.php
@@ -10,7 +10,7 @@ date_default_timezone_set('Asia/Ho_Chi_Minh');
 $vnp_TmnCode = "YOUR_vnp_TmnCode"; //Website ID in VNPAY System
 $vnp_HashSecret = "YOUR_vnp_HashSecret"; //Secret key
 $vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-$vnp_Returnurl = "http://localhost/ThucHanhLapTrinhWeb/web-order-php/vnpay_return.php";
+$vnp_Returnurl = "http://localhost/web-order-php/vnpay_return.php";
 $vnp_apiUrl = "http://sandbox.vnpayment.vn/merchant_webapi/merchant.html";
 //Config input format
 //Expire
