7c72e3a1fbed5200d3fda4bcefd4f9aaba5a0a67
aquoc09 <anhquoc6a4@gmail.com>
2025-12-27 00:39:42 +0700

    demo deploy

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ include 'includes/header.php'; // header + navbar
         <!-- Blog info -->
         <div class="row align-items-start" style="margin-top: 50px;">
             <div class="col-md-7 text-section">
-                <h4><strong>Quán Ăn Nhật Bản</strong></h4>
+                <h4><strong>PQ Restaurant</strong></h4>
                 <p>
                     PQ – Nhà hàng Nhật Bản chuẩn vị giữa lòng thành phố sẽ đưa bạn lạc vào thế giới ẩm thực tinh tế và đậm đà bản sắc của xứ sở hoa anh đào. Nếu đã một lần thưởng thức những món ăn tại PQ, bạn sẽ khó lòng quên được hương vị “say mê” của các món sashimi tươi rói, sushi cuộn tinh tế, hay những lát bò Wagyu nướng chín vừa mềm tan trên đầu lưỡi.
                     Tại PQ, mỗi món ăn đều là một tác phẩm nghệ thuật được chế biến tỉ mỉ, hòa quyện giữa nguyên liệu thượng hạng và phong cách trình bày tinh tế của người Nhật. Không chỉ có món chính, các món kèm như súp miso, tempura giòn rụm, bánh xèo Nhật Bản (okonomiyaki) hay lẩu shabu thanh ngọt cũng góp phần mang lại trải nghiệm ẩm thực trọn vẹn.
@@ -24,8 +24,8 @@ include 'includes/header.php'; // header + navbar
                 <a href="menu.php" class="custom-link">Xem thực đơn</a>
             </div>
             <div class="col-md-5 d-flex flex-row img-section">
+                <img src="./img/pics-info/blog-tamago.png" alt="" class="img-blog-food" >
                 <img src="./img/pics-info/blog-set-curry.jpg" alt="" class="img-blog-food" >
-                <img src="./img/pics-info/blog-sushi.jpg" alt="" class="img-blog-food" >
             </div>
         </div>
 
